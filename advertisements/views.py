22c0cce0a6ffff13a2ab50119428cfcba6847c8a
Nikitaastapov <nikitaastapov@gmail.com>
2023-04-08 08:19:37 +0300

    var3

--- advertisements/views.py
+++ advertisements/views.py
@@ -1,4 +1,4 @@
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework.viewsets import ModelViewSet
 from .models import Advertisement
 from .serializers import AdvertisementSerializer
@@ -6,32 +6,24 @@ from .permissions import IsOwnerOrReadOnly
 from django_filters.rest_framework import DateFromToRangeFilter, FilterSet, DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework import filters
+from .filters import AdvertisementFilter
  
 class CustomSearchFilter(filters.SearchFilter):
     search_param = 'creator'
     
-class F(FilterSet):
-    date = DateFromToRangeFilter()
-    class Meta:
-        model = Advertisement
-        fields = ['created_at',]    
-
 class AdvertisementViewSet(ModelViewSet):
     """ViewSet для объявлений."""
  # TODO: настройте ViewSet, укажите атрибуты для кверисета,
  #   сериализаторов и фильтров
     queryset = Advertisement.objects.all()
     serializer_class = AdvertisementSerializer
-    permission_classes = [IsOwnerOrReadOnly,]
+    permission_classes = [IsOwnerOrReadOnly,IsAuthenticatedOrReadOnly]
     filter_backends = [DjangoFilterBackend, OrderingFilter, CustomSearchFilter]
-    search_fields = ['title', 'description', 'creator__id']
+    search_fields = ['title', 'description', 'creator__id', 'status']
     ordering_fields = ['id','user','created_at']
-    filterset_class = F
+    filterset_class = AdvertisementFilter
 
     
-
-
-
     # def get_permissions(self):
     #     """Получение прав для действий."""
     #     if self.action in ["create", "update", "partial_update"]:
